9f2d32046b732e4fb25c3068ee63b68755955a06
malaika <101466672+malakhims@users.noreply.github.com>
2026-06-24 02:27:54 -0400

    Modify questions table structure and character set

--- questions.sql
+++ questions.sql
@@ -1,23 +1,50 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fanlistings`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
 
 CREATE TABLE `questions` (
-  `question` text NOT NULL,
-  `answer` mediumtext DEFAULT NULL,
-  `visible` text NOT NULL,
+  `question` mediumtext NOT NULL,
+  `answer` longtext DEFAULT NULL,
+  `visible` mediumtext NOT NULL DEFAULT 'n',
   `ID` int(11) NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
   `slug` varchar(255) DEFAULT NULL,
   `tags` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `questions`
+--
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `slug` (`slug`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `questions`
+--
 ALTER TABLE `questions`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
